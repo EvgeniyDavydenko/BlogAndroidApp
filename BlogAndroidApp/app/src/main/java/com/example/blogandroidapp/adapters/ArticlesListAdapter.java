563ca955e8fd9f0fe18d17bfeaef8a605ac7439c
EvgeniyDavydenko <yevgendavydenko@gmail.com>
2020-05-13 00:58:14 +0300

    added TabLayout as first item of RecyclerView

--- BlogAndroidApp/app/src/main/java/com/example/blogandroidapp/adapters/ArticlesListAdapter.java
+++ BlogAndroidApp/app/src/main/java/com/example/blogandroidapp/adapters/ArticlesListAdapter.java
@@ -14,14 +14,24 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.blogandroidapp.R;
 import com.example.blogandroidapp.data.datamodel.Article;
+import com.example.blogandroidapp.data.datamodel.Header;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ArticlesListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
+
     private LayoutInflater layoutInflater;
     private List<Article> articleList = new ArrayList<>();
+    private Header header;
     private Context context;
 
 
@@ -30,46 +40,85 @@ public class ArticlesListAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         layoutInflater = LayoutInflater.from(context);
     }
 
-
     @Override
     public int getItemCount() {
-        return articleList.size();
+        return articleList.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADER;
+        }
+        return TYPE_ITEM;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = layoutInflater.inflate(R.layout.article_item, parent, false);
-        return new ArticleViewHolder(view);
+        if (viewType == TYPE_HEADER) {
+            View view = layoutInflater.inflate(R.layout.tap_bar_item, parent, false);
+            return new HeaderViewHolder(view);
+        } else {
+            View view = layoutInflater.inflate(R.layout.article_item, parent, false);
+            return new ArticleViewHolder(view);
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof HeaderViewHolder) {
+//            ((HeaderViewHolder) holder).headerTabs.getTabAt(0).select();
+        }
         if (holder instanceof ArticleViewHolder) {
+            Article currentItem = articleList.get(position - 1);
+
             Glide.with(context)
-                    .load(articleList.get(position).getPicture())
+                    .load(currentItem.getArticlePicture())
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .skipMemoryCache(false)
                     .dontAnimate()
                     .into(((ArticleViewHolder) holder).articlePicture);
-            ((ArticleViewHolder) holder).articleTitle.setText(articleList.get(position).getTitle());
+
+            ((ArticleViewHolder) holder).articleTitle.setText(currentItem.getTitle());
+            ((ArticleViewHolder) holder).dateArticle.setText(convertTimeStampToDate(currentItem.getDateCreated()));
         }
     }
 
     public void setArticlesData(List<Article> articleList) {
         this.articleList.addAll(articleList);
+        header = new Header(articleList.get(0).getCategory().getId(), articleList.get(0).getCategory().getTitle());
         notifyDataSetChanged();
+    }
+
+    private String convertTimeStampToDate(String dateTimeStamp){
+        long timeStamp = Long.parseLong(dateTimeStamp);
+        Date date = new Date(timeStamp * 1000);
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yy");
+        return dateFormat.format(date);
+    }
+
+    private class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        private TabLayout headerTabs;
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+            headerTabs = itemView.findViewById(R.id.headerTabs);
+        }
     }
 
     private class ArticleViewHolder extends RecyclerView.ViewHolder {
 
         private ImageView articlePicture;
         private TextView articleTitle;
+        private TextView dateArticle;
 
         public ArticleViewHolder(View itemView) {
             super(itemView);
             articlePicture = itemView.findViewById(R.id.articlePicture);
             articleTitle = itemView.findViewById(R.id.articleTitle);
+            dateArticle = itemView.findViewById(R.id.dateArticle);
         }
     }
 
--- BlogAndroidApp/app/src/main/java/com/example/blogandroidapp/data/datamodel/Article.java
+++ BlogAndroidApp/app/src/main/java/com/example/blogandroidapp/data/datamodel/Article.java
@@ -1,5 +1,7 @@
 package com.example.blogandroidapp.data.datamodel;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class Article implements Serializable {
@@ -7,8 +9,11 @@ public class Article implements Serializable {
     private String id;
     private String status;
     private String title;
-    private String picture;
-//    private Category category;
+    @SerializedName("picture")
+    private String articlePicture;
+    @SerializedName("date_created")
+    private String dateCreated;
+    private Category category;
 
     public String getId() {
         return id;
@@ -34,20 +39,28 @@ public class Article implements Serializable {
         this.title = title;
     }
 
-//    public Category getCategory() {
-//        return category;
-//    }
-//
-//    public void setCategory(Category category) {
-//        this.category = category;
-//    }
-
-    public String getPicture() {
-        return picture;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setPicture(String picture) {
-        this.picture = picture;
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getArticlePicture() {
+        return articlePicture;
+    }
+
+    public void setArticlePicture(String articlePicture) {
+        this.articlePicture = articlePicture;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
     }
 
     public class Category implements Serializable {
